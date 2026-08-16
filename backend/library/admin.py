4277d4523c9993062c1a132e00d2f2f9221b8ede
Brian Park <96549640+Cheekane@users.noreply.github.com>
2026-08-16 00:35:49 -0400

    Register PendingDetection with the Django admin site

--- backend/library/admin.py
+++ backend/library/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import LibraryBook
+from .models import LibraryBook, PendingDetection
 
 admin.site.register(LibraryBook)
+admin.site.register(PendingDetection)
